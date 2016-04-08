a82026d492cd4a86715bc3ee51e0d040b171d812
wangcanliang <iamwangcan@163.com>
2016-04-08 17:22:39 +0800

    调整环境配置文件内的配置信息

--- environments/dev/common/config/bootstrap-local.php
+++ environments/dev/common/config/bootstrap-local.php
@@ -1,14 +1,3 @@
 <?php
 Yii::setAlias('assetcustom', 'e:/www/common/asset'); // 前端静态资源路径
 Yii::setAlias('asseturl', 'http://localhost/common/asset'); // 静态资源URL
-Yii::setAlias('asseturlspread', 'http://s2.17house.me/grouponsem'); // 静态资源URL
-//Yii::setAlias('asseturlold', 'http://localhost/workspace/17houseStatic'); // 静态资源URL
-Yii::setAlias('asseturlold', 'http://s1.17house.me'); // 静态资源URL
-Yii::setAlias('backendurl', 'http://yii2.portal.alyee.biz'); 
-Yii::setAlias('passporturl', 'http://yii2.user.alyee.biz'); 
-Yii::setAlias('websiteurl', 'http://yii2.groupbuy.alyee.biz'); 
-Yii::setAlias('spreadurl', 'http://yii2.spread.alyee.biz'); 
-Yii::setAlias('grouponurl', 'http://yii2.groupon.alyee.biz'); 
-Yii::setAlias('merchanturl', 'http://yii2.merchant.alyee.biz'); 
-Yii::setAlias('paytradeurl', 'http://yii2.pay.alyee.biz'); 
-Yii::setAlias('syurl', 'http://yii2.sy.alyee.biz'); 
