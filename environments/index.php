6104886cb021c4eb388987465d0391bfc3cd7953
homework <iamwangcan@163.com>
2016-04-10 07:55:08 +0800

    update environment config for shoot

--- environments/index.php
+++ environments/index.php
@@ -5,10 +5,7 @@ return [
         'setWritable' => [
             'backend/runtime',
             //'backend/web/assets',
-            'passport/runtime',
             'shoot/runtime',
-            'gallerycms/runtime',
-            'spread/runtime',
         ],
         'setExecutable' => [
             'yii',
@@ -16,30 +13,21 @@ return [
         ],
         'setCookieValidationKey' => [
             'backend/config/main-local.php',
-            'passport/config/main-local.php',
             'shoot/config/main-local.php',
-            'gallerycms/config/main-local.php',
-            'spread/config/main-local.php',
         ],
     ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
             'backend/runtime',
-            'passport/runtime',
             'shoot/runtime',
-            'gallerycms/runtime',
-            'spread/runtime',
         ],
         'setExecutable' => [
             'yii',
         ],
         'setCookieValidationKey' => [
             'backend/config/main-local.php',
-            'passport/config/main-local.php',
             'shoot/config/main-local.php',
-            'gallerycms/config/main-local.php',
-            'spread/config/main-local.php',
         ],
     ],
 ];
