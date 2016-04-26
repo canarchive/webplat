ecf18198c47146f251b079657ce735f6e6a20215
homework <iamwangcan@163.com>
2016-04-26 23:57:47 +0800

    repair the bug of gallerycms tag

--- backend/gallerycms/views/tag/listinfo.php
+++ backend/gallerycms/views/tag/listinfo.php
@@ -16,8 +16,8 @@ $gridViewParams = [
                 return '<input name="orderlist" type="text" style="width:30px;" value="' . $model->orderlist . '" class="input-text-c" onchange="updateElemByAjax(\'' . $updateUrl . '\', ' . $model->id . ', \'orderlist\', this.value);">';
             },  
         ],
-		'seo_title',
-		'seo_keyword',
+		'meta_title',
+		'meta_keyword',
 
     ],
 ];
