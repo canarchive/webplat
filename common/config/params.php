e55035c81f0653c7854b4cd0a6d7f1c4cb4bb572
wangcanliang <iamwangcan@163.com>
2016-05-03 18:20:45 +0800

    develop shoot

--- common/config/params.php
+++ common/config/params.php
@@ -9,4 +9,8 @@ return [
 	'bankInfos' => require(__DIR__ . '/bank.php'),
 	'genderInfos' => ['0' => '保密', '1' => '男', '2' => '女'],
 	'siteInfos' => require(__DIR__ . '/site-infos.php'),
+
+	'seoTitle' => '摄影就上摄到家',
+	'seoKeyword' => '摄影o2o,摄影,人像,静物,产品,婚纱',
+	'seoDescription' => '摄到家是一个提供上门摄影的全新摄影平台，简单、快捷、方便',
 ];
