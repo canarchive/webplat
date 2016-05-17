bddebd58705a078e6be683a7f9bf8c9c9e3a1872
wangcanliang <iamwangcan@163.com>
2016-05-17 14:55:01 +0800

    develop cms template

--- gallerycms/components/Controller.php
+++ gallerycms/components/Controller.php
@@ -5,4 +5,26 @@ use common\components\Controller as CommonController;
 
 class Controller extends CommonController
 {
+	public function getArticleCategoryInfos()
+	{
+		static $datas = null;
+
+		if (is_null($infos)) {
+		    $category = new \gallerycms\models\ArticleCategory();
+		    $infos = $category->find()->where(['status' => 1])->indexBy('id')->orderBy(['orderlist' => SORT_DESC])->asArray()->all();
+		}
+
+		$datas = [
+			'infos' => $infos,
+			'levelInfos' => [],
+		];
+		foreach ($infos as $catId => $info) {
+			if (!isset($datas['levelInfos'][$info['parent_id']]) {
+				$datas['levelInfos'][$ifno['parent_id']] = [];
+			}
+			$datas['levelInfos'][$ifno['parent_id']][$catId] = $info;
+		}
+
+		return $datas;
+	}	
 }
