b12dec83c88d15dfac184ba89a41f04012513cca
wangcanliang <iamwangcan@163.com>
2016-06-27 15:35:54 +0800

    develop attachment for spread

--- spread/models/Attachment.php
+++ spread/models/Attachment.php
@@ -7,69 +7,49 @@ use common\models\Attachment as AttachmentBase;
 
 class Attachment extends AttachmentBase
 {
-    /**
-     * @inheritdoc
-     */
-    public static function tableName()
-    {
-        return '{{%attachment}}';
-    }
-
-	public function getFieldInfos($table = null, $field = null)
+	protected function _fieldInfos()
 	{
 		$infos = [
-			'groupon' => [
+			'decoration' => [
 				'picture' => [
         			'isSingle' => true,
     				'minSize' => 1, // unit: kb
-    				'maxSize' => 100,
+    				'maxSize' => 300,
     				'type' => 'image/*',
 				],
 				'picture_small' => [
         			'isSingle' => true,
     				'minSize' => 1, // unit: kb
-    				'maxSize' => 40,
+    				'maxSize' => 200,
     				'type' => 'image/*',
 				],
 				'map' => [
         			'isSingle' => true,
     				'minSize' => 1, // unit: kb
-    				'maxSize' => 100,
+    				'maxSize' => 200,
     				'type' => 'image/*',
-				],				
+				],
 			],
-			'product' => [
-				'main_photo' => [
+			'activity' => [
+				'picture' => [
         			'isSingle' => true,
     				'minSize' => 1, // unit: kb
-    				'maxSize' => 100,
+    				'maxSize' => 300,
     				'type' => 'image/*',
 				],
-			],			
-			'brand' => [
-				'logo' => [
+				'picture_small' => [
         			'isSingle' => true,
     				'minSize' => 1, // unit: kb
-    				'maxSize' => 80,
+    				'maxSize' => 200,
     				'type' => 'image/*',
 				],
-			],			
+				'map' => [
+        			'isSingle' => true,
+    				'minSize' => 1, // unit: kb
+    				'maxSize' => 200,
+    				'type' => 'image/*',
+				],
+			],
 		];
-
-		if (is_null($table) && is_null($field)) {
-			return $infos;
-		}
-
-		if (!isset($infos[$table])) {
-			return false;
-		}
-		if (is_null($field)) {
-			return $infos[$table];
-		}
-		if (!isset($infos[$table][$field])) {
-			return false;
-		}
-
-		return $infos[$table][$field];
 	}
 }
