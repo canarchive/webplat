1da17af17844c26bebbe50829a51427eb7dc08fa
wangcanliang <iamwangcan@163.com>
2016-06-28 14:28:34 +0800

    develop shoot

--- backend/shoot/views/sample/listinfo.php
+++ backend/shoot/views/sample/listinfo.php
@@ -6,6 +6,13 @@ $gridViewParams = [
     'columns' => [
         'id',
 		[
+			'format' => 'raw',
+			'attribute' => 'filepath',
+			'value' => function($model) {
+				return $model->getAttachmentImg($model->id);
+			}
+		],		
+		[
 			'attribute' => 'photographer_id',
 			'value' => function($model) {
 				$info = $model->photographerInfo;
@@ -19,7 +26,15 @@ $gridViewParams = [
 				return isset($info['name']) ? $info['name'] : '';
 			},
 		],
-		'name',
+		[
+            'format' => 'raw',
+            'attribute' => 'name',
+            'value' => function($model) {
+                $appMenus = $this->context->menuInfos['appMenus'];
+                $updateUrl = isset($appMenus['update']) ? $appMenus['update']['url'] : ''; 
+                return '<input name="name" type="text" style="width:100px;" value="' . $model->name . '" class="input-text-c" onchange="updateElemByAjax(\'' . $updateUrl . '\', ' . $model->id . ', \'name\', this.value);">';
+            },  
+        ],
 		[
             'format' => 'raw',
             'attribute' => 'orderlist',
@@ -34,6 +49,15 @@ $gridViewParams = [
             'value'=> function($model){
                 return  date('Y-m-d H:i:s',$model->created_at);
             },
+        ],
+        [   
+            'format' => 'raw',
+            'attribute' => 'description',
+            'value' => function($model) {
+                $appMenus = $this->context->menuInfos['appMenus'];
+                $updateUrl = isset($appMenus['update']) ? $appMenus['update']['url'] : ''; 
+                return '<textarea name="description" onchange="updateElemByAjax(\'' . $updateUrl . '\', ' . $model->id . ', \'description\', this.value);">' . $model->description . '</textarea>';
+            },  
         ],
     ],
 ];
