01ad1098c2dc187c8979720241a232ba29a530b9
wangcanliang <iamwangcan@163.com>
2016-07-05 19:29:41 +0800

    develop merchant

--- backend/merchant/controllers/CompanyController.php
+++ backend/merchant/controllers/CompanyController.php
@@ -10,93 +10,35 @@ use backend\components\AdminController;
 
 class CompanyController extends AdminController
 {
+	protected $modelClass = 'merchant\models\Company';
 
-    /**
-     * Lists all Company.
-     * @return mixed
-     */
     public function actionListinfo()
     {
         $searchModel = new CompanySearch();
-        $dataProvider = $searchModel->search(Yii::$app->request->getQueryParams());
-
-        return $this->render('listinfo', [
-            'dataProvider' => $dataProvider,
-            'searchModel' => $searchModel,
-        ]);
+		return $this->_listinfoInfo($searchModel);
     }
 
-    /**
-     * Displays a single Company model.
-     * @param  string $id
-     * @return mixed
-     */
     public function actionView($id)
     {
-        $model = $this->findModel($id);
-
-        return $this->render('view', ['model' => $model]);
+		return $this->_viewInfo($id);
     }
 
-    /**
-     * Creates a new Company model.
-     * If creation is successful, the browser will be redirected to the 'view' page.
-     * @return mixed
-     */
     public function actionAdd()
     {
-        $model = new Company();
-
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
-
-        return $this->render('add', [
-            'model' => $model,
-        ]);
+		return $this->_addInfo(new Company());
     }
 
-    /**
-     * Updates an existing Company model.
-     * If update is successful, the browser will be redirected to the 'view' page.
-     * @param  string $id
-     * @return mixed
-     */
-    public function actionUpdate($id)
+    public function actionUpdate($id = 0)
     {
-        $model = $this->findModel($id);
-        if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
-        }
+		if (Yii::$app->request->isAjax) {
+		    return $this->_updateByAjax();
+		}
 
-        return $this->render('update', ['model' => $model,]);
+		return $this->_updateInfo($id);
     }
 
-    /**
-     * Deletes an existing Company model.
-     * If deletion is successful, the browser will be redirected to the 'index' page.
-     * @param  string $id
-     * @return mixed
-     */
     public function actionDelete($id)
     {
-        $this->findModel($id)->delete();
-
-        return $this->redirect(['listinfo']);		
-    }
-
-    /**
-     * Finds the Company model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param  string        $id
-     * @return Company      the loaded model
-     * @throws HttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Company::findOne($id)) !== null) {
-            return $model;
-        }
-        throw new NotFoundHttpException('The requested page does not exist.');		
+		return $this->_deleteInfo($id);
     }
 }
