fbeebe8e5b833b1e486e78f9acfbd6f26261bdf2
wangcanjob <iamwangcan@163.com>
2016-07-25 10:31:45 +0800

    develop gallerycms of house

--- gallerycms/house/views/pc/site/_sample.php
+++ gallerycms/house/views/pc/site/_sample.php
@@ -5,11 +5,11 @@ use yii\helpers\Url;
     <h3>装修效果图</h3>
     <p>看海量装修美图，为新家找灵感！</p>
     <div class="items padTop15">
-        <?php foreach ($infos as $info) { ?>
-		<a href="<?= Url::to(['/house/sample/show', 'id' => $info['id']]); ?>" target="_blank">
+        <?php $i = 0; foreach ($infos as $info) { ?>
+		<a <?php if ($i == 3) { echo 'class="last"'; } ?> href="<?= Url::to(['/house/sample/show', 'id' => $info['id']]); ?>" target="_blank">
 		    <img src="<?= $info['thumb']; ?>">
 			<div class="transparent-mask"><?= $info['name']; ?></div>
         </a>
-        <?php } ?>
+        <?php $i++; } ?>
     </div>
 </div>
