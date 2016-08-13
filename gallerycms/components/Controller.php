f7490ac0b4d84bb087ae31633a3f101d5108be48
wangcanjob <iamwangcan@163.com>
2016-08-14 05:39:39 +0800

    develop gallerycms

--- gallerycms/components/Controller.php
+++ gallerycms/components/Controller.php
@@ -4,6 +4,7 @@ namespace gallerycms\components;
 use Yii;
 use yii\helpers\Url;
 use common\components\Controller as CommonController;
+use merchant\models\Company;
 
 class Controller extends CommonController
 {
@@ -14,82 +15,65 @@ class Controller extends CommonController
     {
         parent::init();
 
-        $this->isMobile = $this->clientIsMobile();
 		$this->host = \Yii::$app->request->hostInfo;
-
 		$hostPc = Yii::getAlias('@gallerycmsurl');
 		$hostMobile = Yii::getAlias('@m.gallerycmsurl');
+        //$this->isMobile = $this->clientIsMobile();
+		$this->isMobile = $this->host == $hostMobile ? true : false;
+
+		$url = Yii::$app->request->url;
+		$cityCode = isset($this->module->currentCityCode) ? $this->module->currentCityCode : null;
+		$redirect = strpos($url, 'index.php') !== false ? true : false;
+		$redirect = empty($redirect) ? $this->isMobile && $this->host != $hostMobile : $redirect;
+		$redirect = empty($redirect) ? !$this->isMobile && $this->host == $hostMobile : $redirect;
+		$redirect = empty($redirect) ? $this->host == $hostMobile && is_null($cityCode) && $url == '/' : $redirect;
+		$redirect = empty($redirect) ? !is_null($cityCode) && $cityCode != Yii::$app->params['currentCompany']['code_short'] : $redirect;
+		if ($redirect) {
+			$rule = $this->_redirectRule();
+			$url = Url::to([$rule, 'city_code' => Yii::$app->params['currentCompany']['code_short']]);
+			header("Location:$url");
+		    //return Yii::$app->response->redirect($url)->send();
+			exit();
+		}
 
 		if (isset($this->module->viewPath)) {
 			$this->module->viewPath .= $this->isMobile ? '/mobile' : '/pc';
 		}
-
-		if ($this->isMobile && $this->host != $hostMobile) {
-			return $this->redirect($hostMobile)->send();
-		}
-		if (!$this->isMobile && $this->host != $hostPc) {
-			return $this->redirect($hostPc)->send();
-		}
     }
 
-	protected function getCategoryInfos()
+	public function getTdkInfos($index, $datas = [])
 	{
-		$model = new \gallerycms\models\ArticleCategory();
-		$infos = $model->find()->select('id, parent_id, name, orderlist')->orderBy(['orderlist' => SORT_DESC])->asArray()->all();
+		$default = [
+			'title' => Yii::$app->params['seoTitle'],
+			'keyword' => Yii::$app->params['seoKeyword'],
+			'description' => Yii::$app->params['seoDescription'],
+		];
+		$infos = require(Yii::getAlias('@gallerycms') . '/config/params-tdk.php');
+		$info = isset($infos[$index]) ? $infos[$index] : $default;
 
-		$datas = [];
-		foreach ($infos as $key => $info) {
-			if ($info['parent_id'] == 0) {
-				$datas[$info['id']] = $info;
-				unset($infos[$key]);
-			}
-		}
+		$placeholder = array_merge(
+			[
+				'{{CITYNAME}}', 
+				'{{SITENAME}}',
+				'{{BASETITLE}}',
+				'{{BASEKEYWORD}}',
+				'{{BASEDESCRIPTION}}',
+			], array_keys($datas)
+		);
+		$replace = array_merge(
+			[
+				Yii::$app->params['currentCompany']['name'], 
+				Yii::$app->params['siteName'],
+				$default['title'],
+				$default['keyword'],
+				$default['description'],
+			], array_values($datas)
+		);
+        foreach ($info as $key => $value) {
+            $info[$key] = str_replace($placeholder, $replace, $value);
+        }
 
-		foreach ($infos as $key => $info) {
-			$parentId = $info['parent_id'];
-			if (isset($datas[$parentId])) {
-				$datas[$parentId]['subInfos'][] = $info;
-			}
-		}
-
-		return $datas;
+		Yii::$app->params['tdkInfos'] = $info;
+		return ;
 	}
-
-	protected function _isMobile()
-	{
-		$host = \Yii::$app->request->hostInfo;
-		return $host == \Yii::getAlias('@m-gallerycmsurl') ? true : false;
-	}
-
-	public function getArticleCategoryInfos($catId = null)
-	{
-		static $datas = null;
-
-		if (is_null($datas)) {
-		    $category = new \gallerycms\models\ArticleCategory();
-			$datas = $category->getDatas();
-		}
-
-		if (!is_null($catId)) {
-			return isset($datas[$catId]) ? $datas[$catId] : [];
-		}
-
-		return $datas;
-	}	
-
-	public function getArticleCategoryLevelInfos($parentId = null)
-	{
-		static $datas = null;
-
-		if (is_null($datas)) {
-		    $category = new \gallerycms\models\ArticleCategory();
-			$datas = $category->getLevelDatas();
-		}
-
-		if (!is_null($parentId)) {
-			return isset($datas[$parentId]) ? $datas[$parentId] : [];
-		}
-
-		return $datas;
-	}		
 }
