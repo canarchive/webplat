67118aceec4929111b5c0a5935dc8955e9c33742
homejob <iamwangcan@163.com>
2016-08-16 02:25:31 +0800

    develop passport

--- passport/models/searchs/UserAddress.php
+++ passport/models/searchs/UserAddress.php
@@ -9,11 +9,24 @@ use passport\models\UserAddress as UserAddressModel;
 
 class UserAddress extends UserAddressModel
 {
+    public function rules()
+    {   
+        return [
+            [['user_id'], 'safe'],
+        ];  
+    }  
+
     public function search($params)
     {
         $query = UserAddressModel::find();
-
         $dataProvider = new ActiveDataProvider(['query' => $query]);
+        if ($this->load($params, '') && !$this->validate()) {
+            return $dataProvider;
+        }   
+
+        if (!empty($this->user_id)) {
+            $query->andFilterWhere(['user_id' => $this->user_id]);
+        }   
 
         return $dataProvider;
     }
