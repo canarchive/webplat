96c629d878b4fb66e8a90ba31550acdc889efbef
homejob <iamwangcan@163.com>
2016-09-10 02:10:29 +0800

    develop spread

--- backend/spread/views/custom-service/_form.php
+++ backend/spread/views/custom-service/_form.php
@@ -2,13 +2,13 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
-
 ?>
 
 <div class="menu-form">
 
     <?php $form = ActiveForm::begin(); ?>
     <?= $form->field($model, 'name')->textInput(['maxlength' => 128]) ?>
+    <?= $form->field($model, 'manager_id')->dropDownList($model->managerInfos, ['prompt' => Yii::t('admin-common', 'Select Manager')]); ?>
     <?= $form->field($model, 'code')->textInput() ?>
     <?= $form->field($model, 'mobile')->textInput() ?>
     <?= $form->field($model, 'status')->dropDownList($model->statusInfos, ['prompt' => Yii::t('admin-common', 'Select Status')]); ?>
