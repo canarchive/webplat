bf030f4d8c19e96ae8d72c07f447d3442812e12a
wangcanjob <iamwangcan@163.com>
2016-09-19 07:51:12 +0800

    develop spread

--- spread/controllers/SiteController.php
+++ spread/controllers/SiteController.php
@@ -8,10 +8,9 @@ class SiteController extends SpreadController
 {
     public function actionIndex()
     {
-		session_start();
-		print_r($_SESSION);
-		exit('spread');
-		//$this->redirect();
+		$url = Yii::getAlias('@gallerycmsurl');
+        header("Location:$url");
+		exit();
     }
 
 	public function actionTg()
@@ -74,4 +73,17 @@ class SiteController extends SpreadController
         echo $str;
         exit();
     }
+
+	public function actionHdenter()
+	{
+		$urlSource = Yii::$app->request->get('url_source', '');
+		$url = http_build_query($_GET);
+		if (isset($_GET['url_source'])) {
+			unset($_GET['url_source']);
+		}
+		$queryStr = http_build_query($_GET);
+		$url = $urlSource . '&' . $queryStr;
+		
+		header("Location:$url");
+	}
 }
