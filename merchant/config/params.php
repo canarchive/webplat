c136a6ac09e9267c2f23ee84676e345dd5b510f3
wangcanjob <iamwangcan@163.com>
2016-09-30 17:23:37 +0800

    develop merchant for house

--- merchant/config/params.php
+++ merchant/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-	'seoTitle' => '55JIA商家系统',
-	'seoKeyword' => '55JIA 商家系统',
-	'seoDescription' => '【55JIA商家系统】',
+	'seoTitle' => '55jia商家系统',
+	'seoKeyword' => '55jia 商家系统',
+	'seoDescription' => '【55jia商家系统】',
 ];
