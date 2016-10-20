7be87a811f094fe2a38a97e50fc5efd26423c5e2
wangcancompany <iamwangcan@163.com>
2016-10-21 07:02:18 +0800

    add some codes for common use

--- docs/common_elems/_form.php
+++ docs/common_elems/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+$regionInfo = $model->getRegionInfo($model->region_code);
+$model->region_level1 = isset($regionInfo['parent_code']) ? $regionInfo['parent_code'] : '';
 ?>
 
 <div class="menu-form">
@@ -19,4 +21,25 @@ use yii\bootstrap\ActiveForm;
 	<?= $this->render('@app/views/common/form_button', ['model' => $model]); ?>
     <?php ActiveForm::end(); ?>
 
+
+
+    <?= $form->field($model, 'region_level1')->dropDownList($model->regionSubInfos,
+    [
+        'prompt'=>'--请选择省--',
+        'onchange'=>'
+            //$(".form-group.field-member-area").hide();
+            $.post("' . Url::to(['/region/listinfo']) . '?action=subInfos&parent_code="+$(this).val(),function(data){
+				console.log(data);
+				var htmlContent = "";
+				$.each(data, function(i, v) {
+					htmlContent += "<option value=\"" + i + "\">" + v + "</option>";
+				});
+
+                $("select#company-region_code").html(htmlContent);
+            });',
+    ]) ?>
+    <?= $form->field($model, 'region_code')->dropDownList($model->getRegionSubInfos($model->region_level1),
+    [
+        'prompt'=>'--请选择市--',
+    ]) ?>
 </div>
