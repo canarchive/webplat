0fd7b6dfcac195877f866db649c93eea9847a27b
wangcancompany <iamwangcan@163.com>
2016-10-27 15:37:31 +0800

    develop merchant old

--- merchant/config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-	'seoTitle' => '55jia商家系统',
-	'seoKeyword' => '55jia 商家系统',
-	'seoDescription' => '【55jia商家系统】',
-];
--- merchant/config/rules.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    ['pattern' => Yii::getAlias('@merchanturl') . '/', 'route' => '/site/index', 'suffix' => ''],
-    '/cp-<code:\w+>' => '/product/index',
-	'signin' => 'site/signin',
-	'signup' => 'site/signup',
-
-	'self' => '/info/index',
-];
