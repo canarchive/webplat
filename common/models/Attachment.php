a2568ef59572ffedabc65833158019415ceeda2e
wangcanjob <iamwangcan@163.com>
2016-10-27 11:19:02 +0800

    develop attachment of common

--- common/models/Attachment.php
+++ common/models/Attachment.php
@@ -327,6 +327,7 @@ class Attachment extends BaseModel
 	 */
 	public function deleteInfo($where, $noDeleteIds)
 	{
+		return ;
 		$infos = $this->find()->where($where)->all();
     	foreach ($infos as $info) {
 			if (in_array($info->id, (array) $noDeleteIds)) {
