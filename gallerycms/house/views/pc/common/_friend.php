a5e8dc016c776f5066bc5c50c19ca9b89fddfb91
wangcanjob <iamwangcan@163.com>
2016-11-01 09:00:08 +0800

    develop gallerycms of house

--- gallerycms/house/views/pc/common/_friend.php
+++ gallerycms/house/views/pc/common/_friend.php
@@ -1,13 +1,20 @@
-<!--<div class="links-top">
+<?php
+use yii\helpers\Url;
+?>
+<div class="links-top">
     <ul>
-        <li class=" links-add ">友情链接</li>
-        <li class="">栏目链接</li>
+        <li class=" links-add ">热门城市</li>
+        <!--<li class="">栏目链接</li>
         <li class="">城市链接</li>
         <li class="">热门资讯</li>
-        <li class="">热门图库</li></ul>
-    <a href="/link.html" target="_blank">友链合作 ></a></div>
+		<li class="">热门图库</li>-->
+    </ul>
+	<!--<a href="/link.html" target="_blank">友链合作 ></a>-->
+</div>
 <div class="links-content">
     <div class="links-one" style="display: block;">
-        <a href="http://cizhuan.etaode.com/" title="佛山瓷砖价格" target="_blank">佛山瓷砖价格</a>
-        <a href="" title="装修效果图大全" target="_blank">装修效果图大全</a></div>
-</div>-->
+        <?php foreach (Yii::$app->params['companyInfos'] as $info) { ?>
+		<a href="<?= Url::to(['/house/site/home', 'city_code' => $info['code_short']]); ?>" title="<?= $info['name_full']; ?>"><?= $info['name']; ?>房屋装修</a>
+        <?php } ?>
+    </div>
+</div>
