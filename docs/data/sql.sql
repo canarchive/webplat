4d345e20037af8997a70fb22232cdda49b0ee45d
wangcancompany <iamwangcan@163.com>
2016-11-03 09:44:11 +0800

    develop backend

--- docs/data/sql.sql
+++ docs/data/sql.sql
@@ -22,5 +22,11 @@ UPDATE `wm_merchant` AS `t1`, (SELECT `merchant_id`, COUNT(*) AS `count` FROM `w
 UPDATE `wm_merchant` AS `t1`, (SELECT `merchant_id`, COUNT(*) AS `count` FROM `wm_working` GROUP BY `merchant_id`) as `t2` SET `t1`.`num_working` = `t2`.`count` WHERE `t1`.`id` = `t2`.`merchant_id`;
 UPDATE `wm_merchant` AS `t1`, (SELECT `merchant_id`, COUNT(*) AS `count` FROM `wm_merchant_comment` GROUP BY `merchant_id`) as `t2` SET `t1`.`num_` = `t2`.`count` WHERE `t1`.`id` = `t2`.`merchant_id`;
 
+UPDATE `wm_designer` AS `t1`, `wm_merchant` AS `t2` SET `t1`.`is_joined` = `t2`.`is_joined` WHERE `t1`.`merchant_id` = `t2`.`id`;
+UPDATE `wm_owner` AS `t1`, `wm_merchant` AS `t2` SET `t1`.`is_joined` = `t2`.`is_joined` WHERE `t1`.`merchant_id` = `t2`.`id`;
+UPDATE `wm_merchant_comment` AS `t1`, `wm_merchant` AS `t2` SET `t1`.`is_joined` = `t2`.`is_joined` WHERE `t1`.`merchant_id` = `t2`.`id`;
+UPDATE `wm_realcase` AS `t1`, `wm_merchant` AS `t2` SET `t1`.`is_joined` = `t2`.`is_joined` WHERE `t1`.`merchant_id` = `t2`.`id`;
+UPDATE `wm_working` AS `t1`, `wm_merchant` AS `t2` SET `t1`.`is_joined` = `t2`.`is_joined` WHERE `t1`.`merchant_id` = `t2`.`id`;
+
 UPDATE `wm_merchant` SET `score` = ROUND(4+RAND(), 1), `num_owner` = FLOOR( 50 + RAND() * (500 - 50)), `num_realcase` = FLOOR( 3 + RAND() * (10 - 3)), `num_working` = FLOOR( 1 + RAND() * (10 - 1)), `praise` = FLOOR( 100 + RAND() * (300 - 100)) WHERE 1
 UPDATE `wm_merchant` SET `score` = `score` + 0.4 WHERE `score` < 4.5
